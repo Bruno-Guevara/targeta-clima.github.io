fab52067f074cd7fa4820539f6c3331db184fe44
Bruno-Gva <brunoguevara4.0@gmail.com>
2024-01-27 17:34:52 -0300

    primer commit

---
+++ openweather/src/components/Spinner.js
@@ -0,0 +1,12 @@
+import React from "react";
+import  '../assets/css/Spinner.css';
+
+const Spinner = () => {
+
+    return (
+        <div className="word"></div>
+    );
+
+}
+
+export default Spinner;
